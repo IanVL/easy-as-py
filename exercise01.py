f972cc77dce85cd245755cf914c8d9cb4f4bd5f5
Ian Van Linden <Ian@localhost.localdomain>
2021-03-17 13:41:45 +0100

    Added additional function get_path_depth

--- exercise01.py
+++ exercise01.py
@@ -3,6 +3,10 @@
 import os
 
 def get_path_components(path=None):
+	'''
+	Return the components of a given path or, if not given, the current
+	working directory.
+	'''
 	If path is None:
 		path = os.getcwd()
 	path_compnents = path.split(os.sep)
@@ -10,4 +14,8 @@ def get_path_components(path=None):
 		path_components.remove('')
 	return path_components
 
-print (get_path_components())
+def (get_path_depth):
+	'''
+	Return the number of path components in a given path.
+	'''
+	return len(get_path_components(path))
