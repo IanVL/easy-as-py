b649d1fd390537bf908a47c8ba2659c5f301f42c
Ian Van Linden <Ian@localhost.localdomain>
2021-03-17 13:31:29 +0100

    Added parameter to function

--- exercise01.py
+++ exercise01.py
@@ -2,8 +2,10 @@
 
 import os
 
-def get_path_components():
-	path_components = os.getcwd().split('/')
+def get_path_components(path=None):
+	If path is None:
+		path = os.getcwd()
+	path_compnents = path.split(os.sep)
 	while '' in path_components:
 		path_components.remove('')
 	return path_components
